7e68542fcc9e63045a040144a92f6bae337c52ad
BVK Chaitanya <bvk@localhost.localdomain>
2025-12-10 12:19:02 -0600

    Added edit order mechanism to the coinbase internal client.

--- coinbase/internal/client_test.go
+++ coinbase/internal/client_test.go
@@ -18,7 +18,9 @@ import (
 var (
 	testingKey     string
 	testingSecret  string
-	testingOptions *Options = &Options{}
+	testingOptions *Options = &Options{
+		MaxFetchTimeLatency: time.Minute,
+	}
 )
 
 func checkCredentials() bool {
@@ -116,4 +118,90 @@ func TestClient(t *testing.T) {
 	}()
 
 	time.Sleep(30 * time.Second)
+}
+
+func TestEditOrder(t *testing.T) {
+	if !checkCredentials() {
+		t.Skip("no credentials")
+		return
+	}
+
+	topic := topic.New[*Message]()
+	defer topic.Close()
+
+	c, err := New(context.Background(), testingKey, testingSecret, testingOptions)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := c.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	ctx := context.Background()
+	prodsResp, err := c.ListProducts(ctx, "SPOT")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Find the current price for BTC-USD.
+	var btcPrice decimal.Decimal
+	for _, p := range prodsResp.Products {
+		if p.ProductID == "BTC-USD" {
+			btcPrice = p.Price.Decimal
+			break
+		}
+	}
+	if btcPrice.IsZero() {
+		t.Fatalf("could not determine current BTC-USD price")
+	}
+	t.Logf("current BTC-USD price is %s", btcPrice)
+
+	// Pick a price-point for the order that is 20% below the current price.
+	product, size, price := "BTC-USD", 0.001, btcPrice.Mul(decimal.NewFromFloat(0.80)).Round(2)
+
+	createReq := &CreateOrderRequest{
+		ClientOrderID: uuid.New().String(),
+		ProductID:     product,
+		Side:          "BUY",
+		Order: &OrderConfig{
+			LimitGTC: &LimitLimitGTC{
+				BaseSize:   exchange.NullDecimal{Decimal: decimal.NewFromFloat(size)},
+				LimitPrice: exchange.NullDecimal{Decimal: price},
+				PostOnly:   true,
+			},
+		},
+	}
+	createResp, err := c.CreateOrder(ctx, createReq)
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		js, _ := json.MarshalIndent(createResp, "", "  ")
+		t.Logf("%s", js)
+	}
+	if !createResp.Success {
+		t.Fatalf("create order is not successful")
+	}
+	defer func() {
+		cancelReq := &CancelOrderRequest{
+			OrderIDs: []string{createResp.SuccessResponse.OrderID},
+		}
+		if _, err := c.CancelOrder(ctx, cancelReq); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	editReq := &EditOrderRequest{
+		OrderID: createResp.SuccessResponse.OrderID,
+		Price:   exchange.NullDecimal{Decimal: price},
+		Size:    exchange.NullDecimal{Decimal: decimal.NewFromFloat(size * 2)},
+	}
+	editResp, err := c.EditOrder(ctx, editReq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !editResp.Success {
+		t.Fatalf("edit order is not successful")
+	}
+	t.Logf("edit order is successful")
 }
